a63e1d8793bbac922623aad3455cfa86053a0f7c
Jakub Dugovic <jakubdugovic@gmail.com>
2023-10-05 22:28:52 +0200

    feat: redux base setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,8 @@
-
-// theming
-import { theme } from './style/theme'
-import { ThemeProvider } from 'styled-components'
-
 import Controls from "./components/compound/Controls"
 
 function App() {
     return (
-        <ThemeProvider theme={theme}>
-            <Controls />
-        </ThemeProvider>
+        <Controls />
     )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+// components
 import App from './App.tsx'
+
+// redux
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
+
+// styles
+import { theme } from './style/theme'
+import { ThemeProvider } from 'styled-components'
 import './style/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <Provider store={store}>
+            <ThemeProvider theme={theme}>
+                <App />
+            </ThemeProvider>
+        </Provider>
+    </React.StrictMode>,
 )
